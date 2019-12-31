46f3fc5b99d68e98b7f6782433b9109226c792f8
Jonghyun Lee <green198800@yonsei.ac.kr>
2019-12-31 23:03:53 +0900

    merge repositories

---
+++ TensorFlow_study/README.md
@@ -0,0 +1,3 @@
+# Tensorflow 2.0.0 Keras 
+
+keras 연습
---
+++ django_poll_app/mysite/elections/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'elections'
+urlpatterns = [
+	path('', views.index, name = "home"),
+	path('areas/<str:area>/', views.areas),
+	path('areas/<str:area>/results', views.results),
+	path('polls/<int:poll_id>/', views.polls)
+]
